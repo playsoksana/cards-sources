1ab43758eecab646c865fc3d47a2eeceed4d3b1e
playsoksana <playsoksana@gmail.com>
2021-09-30 21:39:11 +0300

    add active card default

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const slides = document.querySelectorAll(".slide");
+const slidesPlugin = (num = 2) => {
+  const slides = document.querySelectorAll(".slide");
+  slides[num].classList.add("active");
 
-const clearActiveClasses = () => {
-  slides.forEach((el) => {
-    el.classList.remove("active");
-  });
+  const clearActiveClasses = () => {
+    slides.forEach((el) => {
+      el.classList.remove("active");
+    });
+  };
+
+  const addClassActive = (slide) => {
+    clearActiveClasses();
+    slide.classList.add("active");
+  };
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => addClassActive(slide));
+  }
 };
-
-const addClassActive = (slide) => {
-  clearActiveClasses();
-  slide.classList.add("active");
-};
-
-for (const slide of slides) {
-  slide.addEventListener("click", () => addClassActive(slide));
-}
+slidesPlugin();
